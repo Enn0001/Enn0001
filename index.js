9277f511244510e319921f127186f784e44bca15
Enn0001 <enriconavarro190@gmail.com>
2024-06-08 15:49:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
 }
-function clickLike2() {
+function clickLike2(){
   let totalLikes2 = parseInt(countLikes2.value) + 1
   countLikes2.textContent = totalLikes2.toString()
 }
@@ -16,7 +16,7 @@ const btnDislike2 = document.getElementById("btnDislike2")
 const countDislike2 = document.getElementById("countDislike2")
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
-function clickDislike1() {
+function clickDislike1(){
    let totalDislike1 = parseInt(countDislike1.value) + 1
    countDislike1.textContent = totalDislike1.toString() 
 }
