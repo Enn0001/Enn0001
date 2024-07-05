23e54c7c82d429ae3895cbafaf073bc2259776f3
Enn0001 <enriconavarro190@gmail.com>
2024-07-05 16:24:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function submitComment() {
 }
 submit.addEventListener("click",submitComment)
 -->
-<!- var product1 = document.getElementById("product1")
+var product1 = document.getElementById("product1")
             var qty1 = document.getElementById("qty1")
             var price1 = document.getElementById("price1")
 
